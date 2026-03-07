0405fca20d7bfd075d9d38d4af23ef18cc20191f
marri sree harsha <marriharsha2@gmail.com>
2026-03-07 17:55:00 +0530

    update netlify config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
-  },
+    serverActions: {}
+  }
   images: {
     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
   },
